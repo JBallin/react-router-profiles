803a0e544840960e715ba974353a5585e5c45fc4
JBallin <JBallin@users.noreply.github.com>
2018-11-06 12:02:25 -0800

    Add spinner and error handling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import React from 'react'
 import AppRouter from './AppRouter'
 import TopNav from './components/TopNav'
 import './App.css'
+import Spinner from './components/Spinner'
 
-fetch(process.env.REACT_APP_API_URL);
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    error: false,
+  }
 
-export const App = () => {
-  return (
-    <div>
-      <TopNav />
-      <AppRouter />
-    </div>
-  )
+  async componentDidMount() {
+    const delay = 700;
+    const delayedPromise = new Promise(resolve => setTimeout(resolve, delay));
+    const testFetch = fetch(process.env.REACT_APP_API_URL);
+    try {
+      await Promise.all([ testFetch, delayedPromise ]);
+    }
+    catch (err) {
+      this.setState({error: `Error fetching API`})
+    }
+    this.setState({ isLoading: false });
+  }
+
+  render () {
+
+    const error = (
+      <div className="container mt-4">
+        <h3>{this.state.error}</h3>
+      </div>
+    );
+
+    return (
+      <div>
+        <TopNav />
+        { this.state.isLoading ? <Spinner /> : this.state.error ? error : <AppRouter />}
+      </div>
+    )
+  }
 }
 
 export default App
