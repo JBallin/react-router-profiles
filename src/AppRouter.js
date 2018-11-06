1bc74a76b324054399e0b2b20872ac9cc0ef0da8
JBallin <JBallin@users.noreply.github.com>
2018-11-05 18:41:32 -0800

    Use Switch to render 404 No Match when path doesn't match routes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import { Container } from 'reactstrap'
 
 import Login from './components/Login'
 import Signup from './components/Signup'
 import UserProfile from './components/UserProfile'
 
+const NoMatch = () => (
+  <Container className="main-wrapper mt-5">
+    <h3>404: Not Found</h3>
+  </Container>
+);
+
 const AppRouter = () => (
-  <div>
-    <Route exact path='/' render={() => <Redirect to="/login" />} />
+  <Switch>
+    <Route path='/' exact render={() => <Redirect to="/login" />} />
     <Route path='/login' component={Login} />
     <Route path='/signup' component={Signup} />
     <Route path='/profile' component={UserProfile} />
-  </div>
+    <Route component={NoMatch} />
+  </Switch>
 );
 
 export default AppRouter;
